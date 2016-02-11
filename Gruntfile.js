69687325f9de3c4018a3563da0ff462da2c95025
Johan <rastikkko@gmail.com>
2016-02-11 08:28:19 -0800

    Initial project setup

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: '<your e-mail>',
+                password: '<your password>',
+                branch: 'development',
+                ptr: false
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        }
+    });
+}
